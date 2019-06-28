6762f0478929e18cc39bb9432fffcc0319416886
WuXJ <992622784@qq.com>
2019-06-28 18:19:09 +0800

    commit

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,32 +1,39 @@
 import Taro, {Component} from '@tarojs/taro'
-import {View, Text} from '@tarojs/components'
-//import './Home.less'
+import {View} from '@tarojs/components'
+import { AtInput } from "taro-ui"
+
+import './home.less'
 
 export default class Home extends Component {
 
   config = {
-    navigationBarTitleText: 'Home'
+    navigationBarTitleText: '主页'
   }
 
-  componentWillMount() {
+  constructor(){
+    super()
+    this.state={
+      keyword:'',
+      count:1000,
+    }
   }
 
-  componentDidMount() {
-  }
-
-  componentWillUnmount() {
-  }
-
-  componentDidShow() {
-  }
-
-  componentDidHide() {
+  search=()=>{
+      console.log(this.state.keyword)
   }
 
   render() {
+    const state=this.state;
+
     return (
       <View>
-        <Text>Home</Text>
+        <AtInput className='search'
+          clear
+          type='text'
+          placeholder={`搜索商品,共${state.count}好物`}
+          value={state.keyword}
+          onChange={this.search}
+        />
       </View>
     )
   }
